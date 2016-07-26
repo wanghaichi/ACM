a2b8bcc267bbe92e9d9420adb8daa4930ff19fa6
wanghaichi <703007993@qq.com>
2016-07-26 19:13:16 +0800

    第三场多校

--- 网络流/sap模板.cpp
+++ 网络流/sap模板.cpp
@@ -1,75 +1,155 @@
-#include <iostream>
 #include <cstdio>
+#include <cmath>
 #include <cstring>
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
+const int inf = 0x3f3f3f3f;
+const int maxv = 100047, maxe = 400047;
 
-const int maxe = 100000, maxv = 100000, inf = 1e9;
 
-int ne, nv, tmp, s, t, index;
-struct node{
-	int next, pair, v, cap, flow;
-}e[maxe];
+struct E
+{
+    int to, from, next, cap;
+}edge[maxe];
 
-int net[maxv];
+int head[maxv], cnt, n, m, src, des;
+int dep[maxv], gap[maxv];
 
-int isap(){
-	int numb[maxv], dist[maxv], curedge[maxv], pre[maxv];
-	int cur_flow, max_flow, u, tmp, neck, i;
-	memset(dist, 0, sizeof(dist));
-	memset(numb, 0, sizeof(numb));
-	memset(pre, -1, sizeof(pre));
-	for(int i = 1; i <= nv; i ++){
-		curedge[i] = net[i];
-	}
-	numb[nv] = nv;
-	max_flow = 0;
-	u = s;
-	while(dist[s] < nv){
-		if(u == t){
-			cur_flow = inf;
-			for(i = s; i != t; i = e[curedge[i]].v){
-				if(cur_flow > e[curedge[i]].cap){
-					neck = i;
-					cur_flow = edge[curedge[i]].cap;
-				}
-			}
-			for(i = s; i != t; i = e[curedge[i]].v){
-				tmp = curedge[i];
-				e[tmp].cap -= cur_flow;
-				e[tmp].flow += cur_flow;
-				tmp = e[tmp].pair;
-				e[tmp].cap += cur_flow;
-				e[tmp].flow -= cur_flow;
-			}
-			max_flow += cur_flow;
-			u = neck;
-		}
-		for(i = curedge[u]; i != -1; i = e[i].next)
-			if(edge[i].cap > 0 && dist[u] == dist[e[i].v] + 1) break;
-		if(i != -1){
-			curedge[u] = i;
-			pre[edge[i].v] = u;
-			u = e[i].v;
-		}
-		else{
-			if(0 == --numb[dist[u]]) break;
-			curedge[u] = net[u];
-			for(tmp = nv, i = net[u]; i != -1; i = e[i].next){
-				if(e[i].cap > 0){
-					tmp = tmp < dist[e[i].v] ? tmp : dist[edge[i].v];
-				}
-			}
-			dist[u] = tmp + 1;
-			++numb[dist[u]];
-			if(u != s) u = pre[u];
-		}
-	}
-	return max_flow;
+void addedge(int _u, int _v, int _w)
+{
+    edge[cnt].from = _u;
+    edge[cnt].to = _v;
+    edge[cnt].cap = _w;
+    edge[cnt].next = head[_u];
+    head[_u] = cnt++;
+    edge[cnt].from = _v;
+    edge[cnt].to = _u;
+    edge[cnt].cap = 0;
+    edge[cnt].next = head[_v];
+    head[_v] = cnt++;
 }
 
+int que[maxv];
 
+void bfs()
+{
+    memset(dep, -1, sizeof(dep));
+    memset(gap, 0, sizeof(gap));
+    gap[0] = 1;
+    int front = 0, rear = 0;
+    dep[des] = 0;
+    que[rear++] = des;
+    int u, v;
+    while (front != rear)
+    {
+        u = que[front++];
+        front = front % maxv;
+        for (int i=head[u]; i!=-1; i=edge[i].next)
+        {
+            v = edge[i].to;
+            if (edge[i].cap != 0 || dep[v] != -1)
+                continue;
+            que[rear++] = v;
+            rear = rear % maxv;
+            ++gap[dep[v] = dep[u] + 1];
+        }
+    }
+}
+int cur[maxv],stack[maxv];
+int Sap()       //sap模板
+{
+    int res = 0;
+    bfs();
+    int top = 0;
+    memcpy(cur, head, sizeof(head));
+    int u = src, i;
+    while (dep[src] < n)
+    {
+        if (u == des)
+        {
+            int temp = inf, inser = n;
+            for (i=0; i!=top; ++i)
+                if (temp > edge[stack[i]].cap)
+                {
+                    temp = edge[stack[i]].cap;
+                    inser = i;
+                }
+            for (i=0; i!=top; ++i)
+            {
+                edge[stack[i]].cap -= temp;
+                edge[stack[i]^1].cap += temp;
+            }
+            res += temp;
+            top = inser;
+            u = edge[stack[top]].from;
+        }
 
+        if (u != des && gap[dep[u] -1] == 0)
+            break;
+        for (i = cur[u]; i != -1; i = edge[i].next)
+            if (edge[i].cap != 0 && dep[u] == dep[edge[i].to] + 1)
+                break;
 
+        if (i != -1)
+        {
+            cur[u] = i;
+            stack[top++] = i;
+            u = edge[i].to;
+        }
+        else
+        {
+            int min = n;
+            for (i = head[u]; i != -1; i = edge[i].next)
+            {
+                if (edge[i].cap == 0)
+                    continue;
+                if (min > dep[edge[i].to])
+                {
+                    min = dep[edge[i].to];
+                    cur[u] = i;
+                }
+            }
+            --gap[dep[u]];
+            ++gap[dep[u] = min + 1];
+            if (u != src)
+                u = edge[stack[--top]].from;
+        }
+    }
+    return res;
+}
 
-
+int main()
+{
+    int T, i;
+    scanf("%d", &T);
+    while (T--)
+    {
+        scanf("%d%d", &n, &m);
+        int x, y;
+        int Min = inf, Max = -inf;
+        for (i=1; i<=n; ++i) {      //找出起点src 终点des
+            scanf("%d%d", &x, &y);
+            if (x <= Min){
+                src = i;
+                Min = x;
+            }
+            if (x >= Max){
+                des = i;
+                Max = x;
+            }
+        }
+        cnt = 0;
+        memset(head, -1, sizeof(head));
+        int u, v, c;
+        for (i=0; i!=m; ++i)
+        {
+            scanf("%d%d%d", &u, &v, &c);
+            addedge(u,v,c);
+            addedge(v,u,c);
+        }
+        int ans = Sap();
+        printf("%d\n", ans);
+    }
+    return 0;
+}
