1c8a01bb204db7dbc043e421d75f53412c0ecf3b
wanghaichi <703007993@qq.com>
2016-08-05 13:50:19 +0800

    bash创建新的cpp文件

---
+++ newfile.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo -e "/**************************************************************\n" > b.cpp
+
+echo -e "AUTHOR: Hardy\n" >> b.cpp
+
+echo -ne 'TIME: ' >> b.cpp
+
+echo `date` >> b.cpp
+
+echo -e "\nID:\n" >> b.cpp
+
+echo -e "TYPE:\n" >> b.cpp
+
+echo -e "DETAIL:\n" >> b.cpp
+
+echo -e "TACTICS:\n" >> b.cpp
+
+echo -e "***************************************************************/\n" >> b.cpp
+
+echo '#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#define MAX(a, b) (a > b ? a : b)
+#define MIN(a, b) (a > b ? b : a)
+#define CL(a) memset(a, 0, sizeof(a));
+
+using namespace std;
+
+typedef long long ll;
+
+const int inf = 1e9+7;
+const int mod = 1e9+7;
+const int maxn = 1e6+7, maxe = 1e6+7, maxv = 1e6+7;
+
+int main()
+{
+	
+	
+	return 0;
+}' >> b.cpp
+
+echo -e "\n\n\n\n\n\n\n" >> b.cpp
